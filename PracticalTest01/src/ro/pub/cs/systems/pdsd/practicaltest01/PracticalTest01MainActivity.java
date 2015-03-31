9eb1c88dc06644977afc852db27b3c4715ef53fa
Secui Ana <secui.ana1@gmail.com>
2015-03-31 08:46:14 +0300

    a

---
+++ PracticalTest01/src/ro/pub/cs/systems/pdsd/practicaltest01/PracticalTest01MainActivity.java
@@ -0,0 +1,111 @@
+package ro.pub.cs.systems.pdsd.practicaltest01;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+
+public class PracticalTest01MainActivity extends Activity {
+	
+	Button b1;
+	EditText t1, t2;
+	CheckBox c1, c2;
+	
+	private TextListener textL = new TextListener();
+	
+	public class TextListener implements TextWatcher{
+
+		@Override
+		public void afterTextChanged(Editable s) {
+			
+			
+		}
+
+		@Override
+		public void beforeTextChanged(CharSequence s, int start, int count,
+				int after) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before,
+				int count) {
+			t1 = (EditText) findViewById(R.id.text1);
+			t2 = (EditText) findViewById(R.id.text2);
+			c1 = (CheckBox) findViewById(R.id.check1);
+			c2 = (CheckBox) findViewById(R.id.check2);
+			
+			if(t1.getText().toString() != null){
+				if(isEmailValid(t1.getText().toString())){
+					c1.setChecked(true);
+				}
+			}
+			
+			if(t2.getText().toString() != null){
+				if(t1.getText().toString().length() == 10 && t1.getText().toString().matches("[0-9]+")){
+					c1.setChecked(true);
+				}
+			}
+			
+		}
+		
+	}
+	
+	public static boolean isEmailValid(String email) {
+	    boolean isValid = false;
+
+	    String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+	    CharSequence inputStr = email;
+
+	    Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+	    Matcher matcher = pattern.matcher(inputStr);
+	    if (matcher.matches()) {
+	        isValid = true;
+	    }
+	    return isValid;
+	}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_main);
+        
+        t1 = (EditText) findViewById(R.id.text1);
+		t2 = (EditText) findViewById(R.id.text2);
+		c1 = (CheckBox) findViewById(R.id.check1);
+		c2 = (CheckBox) findViewById(R.id.check2);
+		
+		t1.addTextChangedListener(textL);
+		t2.addTextChangedListener(textL);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.practical_test01_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
